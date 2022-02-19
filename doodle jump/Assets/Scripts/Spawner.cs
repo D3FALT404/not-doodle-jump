6a392fe0cea117ef2ffb38520819fc41dd01b4c9
D3FALT404 <klwid04@gmail.com>
2022-02-19 14:22:53 +0100

    1.5

--- doodle jump/Assets/Scripts/Spawner.cs
+++ doodle jump/Assets/Scripts/Spawner.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
+    [SerializeField] private GameObject platform;
+    [SerializeField] private int platfomsCount;
+    private float distanceLastPlatform = 0;
+
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+        spawnPlatforms();
+    }
+
+    private void spawnPlatforms()
+    {
+        for(int i = 0; i < platfomsCount; i++)
+        {
+            float randomX = Random.Range(-6f, 6f);
+            float randomY = Random.Range(1, 3.5f);
+            distanceLastPlatform += randomY;
+            Instantiate(platform, new Vector3(randomX, distanceLastPlatform), Quaternion.identity);
+        }
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
     }
 }
